9364167618fe809fdcba817216fd9dae08128ffc
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2022-09-20 23:43:07 +0200

    20/9/2022 ebrahim

--- lib/services/home/pages/request_details.dart
+++ lib/services/home/pages/request_details.dart
@@ -3,7 +3,22 @@ import 'package:thermo_ieee_app/services/home/widgets/requests/details_card.dart
 import 'package:thermo_ieee_app/services/notification/widgets/circleimage.dart';
 
 class RequestDetails extends StatelessWidget {
-  const RequestDetails({Key? key}) : super(key: key);
+
+  String name;
+  String discription;
+  String? date;
+  String? location;
+  //Function func;
+
+  RequestDetails({
+    required this.name,
+    required this.discription,
+    required this.date,
+    required this.location,
+    //  required this.func,
+
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +29,12 @@ class RequestDetails extends StatelessWidget {
         iconTheme: IconThemeData(color: Colors.black),
         elevation: 0,
       ),
-      body: Center(child: DetailsCard())
+      body: Center(child: DetailsCard(
+        name: "snapshot.data!.docs[index].get('name')",
+        discription: "snapshot.data!.docs[index].get('description')",
+        date: "snapshot.data!.docs[index].get('create_data')",
+        location: "snapshot.data!.docs[index].get('location')",
+      ))
     );
   }
 }
