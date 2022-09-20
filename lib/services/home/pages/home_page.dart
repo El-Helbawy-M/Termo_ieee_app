64dcae3a1fb7f99d7e41044b7b1654a164ecc8fb
EbrahimMShaaban <ebrahimmsh2019@gmail.com>
2022-09-20 18:53:34 +0200

    { 20/9/2022 Ebrahim - add card details - edit info item size }

--- lib/services/home/pages/home_page.dart
+++ lib/services/home/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:thermo_ieee_app/helpers/colors.dart';
 import 'package:thermo_ieee_app/helpers/constraints.dart';
 import 'package:thermo_ieee_app/helpers/icons.dart';
 import 'package:thermo_ieee_app/helpers/text_styles.dart';
+import 'package:thermo_ieee_app/services/home/pages/request_details.dart';
 import '../../../componants/custom_appbar.dart';
 import '../widgets/categories/category_panel.dart';
 import '../widgets/requests/requests_list.dart';
@@ -18,29 +19,36 @@ class HomePage extends StatelessWidget {
       height: MediaQueryHelper.height,
       child: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal:24),
+          padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children:  [
+            children: [
               // app bar
               CustomAppBar(
                 leadigIcon: customImageIconSVG(imageName: AppIcons.search),
-                actionIcon: customImageIconSVG(imageName: AppIcons.notification_outlined),
+                actionIcon: customImageIconSVG(
+                    imageName: AppIcons.notification_outlined),
               ),
               //===========================
               // categories panel
-              Text("Categories",style: AppTextStyles.w700.copyWith(fontSize: 24,color: AppColors.mainColor)),
+              Text("Categories",
+                  style: AppTextStyles.w700
+                      .copyWith(fontSize: 24, color: AppColors.mainColor)),
               //===========================
               const Padding(
-                padding: EdgeInsets.symmetric(vertical:24),
-                child:  CategoriesPanel(),
+                padding: EdgeInsets.symmetric(vertical: 24),
+                child: CategoriesPanel(),
               ),
               const SizedBox(height: 24),
               //===========================
-              Text("Requests List",style: AppTextStyles.w700.copyWith(fontSize: 24,color: AppColors.mainColor)),
+              Text("Requests List",
+                  style: AppTextStyles.w700
+                      .copyWith(fontSize: 24, color: AppColors.mainColor)),
               const SizedBox(height: 24),
               //===========================
-              const RequstsList(),
+              RequstsList(
+
+              ),
             ],
           ),
         ),
@@ -48,7 +56,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
-
-
-
