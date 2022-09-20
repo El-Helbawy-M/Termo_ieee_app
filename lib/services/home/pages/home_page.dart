6e3b61b542e4c4103321430a4e4ed9f734c2d828
signup <mansterghj@gmail.com>
2022-09-20 19:57:55 +0200

    20/9/2022 hussein

--- lib/services/home/pages/home_page.dart
+++ lib/services/home/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:thermo_ieee_app/helpers/colors.dart';
 import 'package:thermo_ieee_app/helpers/constraints.dart';
 import 'package:thermo_ieee_app/helpers/icons.dart';
 import 'package:thermo_ieee_app/helpers/text_styles.dart';
-import 'package:thermo_ieee_app/services/home/pages/request_details.dart';
+import 'package:thermo_ieee_app/services/home/widgets/worker/worker_list.dart';
 import '../../../componants/custom_appbar.dart';
 import '../widgets/categories/category_panel.dart';
 import '../widgets/requests/requests_list.dart';
@@ -29,26 +29,23 @@ class HomePage extends StatelessWidget {
                 actionIcon: customImageIconSVG(
                     imageName: AppIcons.notification_outlined),
               ),
-              //===========================
               // categories panel
               Text("Categories",
                   style: AppTextStyles.w700
                       .copyWith(fontSize: 24, color: AppColors.mainColor)),
-              //===========================
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 24),
                 child: CategoriesPanel(),
               ),
               const SizedBox(height: 24),
-              //===========================
               Text("Requests List",
                   style: AppTextStyles.w700
                       .copyWith(fontSize: 24, color: AppColors.mainColor)),
               const SizedBox(height: 24),
-              //===========================
-              RequstsList(
 
-              ),
+              const WorkerList(),
+
+              RequstsList(),
             ],
           ),
         ),
